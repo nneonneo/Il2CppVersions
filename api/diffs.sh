992390fa733ceb0a59298a9beb8df75251a3e00f
Sam Byass <sam@samboycoding.me>
2023-03-18 05:55:36 +0000

    Fix script 2023 versions + dump 2023.2.0a6

--- api/diffs.sh
+++ api/diffs.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 vers=($(ls *.h | sed s/a/.a./g | sed s/b/.b./g | sed s/f/.f./g | sed s/p/.p./g | sort -t. -k1,1n -k2,2n -k3,3n -k4,4d -k5,5n | sed s/.a./a/g | sed s/.b./b/g | sed s/.f./f/g | sed s/.p./p/g))
 rm -f diffs.diff
 for ((i=0; i<${#vers[@]}-1; i++)); do
